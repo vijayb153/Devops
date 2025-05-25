f48675a8077d0e61dc1b4a61d1a66253fef060cc
vijay <vijay35777@gmail.com>
2025-05-25 16:38:31 +0530

    update

---
+++ shellscript/03-variablesArg.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "hello $PERSON1"
+echo "hello $PERSON2"
+
+echo "$PERSON1 and $PERSON2 are learning shell scripting!"
+echo "Good luck, $PERSON1 and $PERSON2!"
