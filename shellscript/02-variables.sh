879432d567027d970cf165fc4639d2469b2ec28f
vijay <vijay35777@gmail.com>
2025-05-25 16:25:32 +0530

    update

--- shellscript/02-variables.sh
+++ shellscript/02-variables.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-PERSON1 ="VIJJ"
-PERSON2 ="VINU"
-eco "hello $PERSON1"
-eco "hello $PERSON2"
-eco "hello word"
-eco "hello word"
+
+PERSON1="Alice"
+PERSON2="Bob"
+
+echo "Hello $PERSON1"
+echo "Hello $PERSON2"
+echo "$PERSON1 and $PERSON2 are learning shell scripting!"
+echo "Good luck, $PERSON1 and $PERSON2!"
