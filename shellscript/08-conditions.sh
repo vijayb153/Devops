145a8662ae32c79d27a4d4d8ec0116bf89841679
vijay <vijay35777@gmail.com>
2025-05-25 22:01:32 +0530

    update

--- shellscript/08-conditions.sh
+++ shellscript/08-conditions.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 Number=$1
 
-if[Number -lt 10]
+if[ $Number -lt 10 ]
 then
-{
     echo "given number:{$Number} is less than 10"
-
-}
 else
-{
  echo "given number:{$Number} is morethan than 10"
-
-}
 fi
