b682ae1671f04fbe37e1175a8c1ff5901dea5def
vijay <vijay35777@gmail.com>
2025-05-27 13:25:37 +0530

    update

--- shellscript/10-functions.sh
+++ shellscript/10-functions.sh
@@ -1,22 +1,22 @@
 #!/bin/bash/
 UESERID=$(id -u)
-install $UESERID $1
+install $UESERID "mysql"
 install(){
-if [ $2 -ne 0 ]
+if [ $1 -ne 0 ]
 then
     echo "Error:: pls run as root"
     exit 1
 else
     echo "you are with root access"
 fi
-dnf list installed $1
+dnf list installed $2
 if [ $? -ne 0 ]
 then 
     echo "mysql not installed installing"
-    dnf install $1 -y
-    AVALIDATE $? "$1"
+    dnf install $2 -y
+    AVALIDATE $? "$2"
 else
-    echo "$1 i already installed ...nothing to do"
+    echo "$2 i already installed ...nothing to do"
 fi
 AVALIDATE (){
     if [ $2 -eq 0 ]
